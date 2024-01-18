8253ec71e1dabab9a5ed2a86818e180939c573e0
David Inga <david.inga@vizzuality.com>
2024-01-18 10:50:05 +0100

    fixed basemap provider urls

--- src/components/controls/basemap/constants.ts
+++ src/components/controls/basemap/constants.ts
@@ -3,16 +3,22 @@ import type { BasemapProvider } from './types';
 export const basemapProviders: BasemapProvider[] = [
   {
     name: 'OpenStreetMap',
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 19,
   },
   {
-    name: 'OpenTopoMap',
-    url: 'https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png',
+    name: 'Stadia Stamen WaterColor',
+    url: 'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg',
     attribution:
-      'Map <a href="https://memomaps.de/">memomaps.de</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 18,
+      '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 16,
+  },
+  {
+    name: 'CartoDB Voyager',
+    url: 'https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   },
   {
     name: 'ESRI World Imagery',
